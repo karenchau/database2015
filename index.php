7edf912a13889c279d9a8817311116e4631cc614
karenchau <kchau@smith.edu>
2015-02-09 13:42:32 +0000

    Login
    Minor change

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
     <form>
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
+        <label for="exampleInputEmail1">Email</label>
         <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
       </div>
       <div class="form-group">
