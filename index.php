7f8bcc9b5383ee4b7f61dc920846ad00fa5c3561
karenchau <kchau@smith.edu>
2015-03-06 14:25:10 +0000

    Revert 22c1248..324a55a
    This rolls back to commit 22c12483527a94b03cd04502c6d459abb421bfdc.

--- index.php
+++ index.php
@@ -2,24 +2,20 @@
 <html>
   <head>
     <link type='text/css' rel='stylesheet' href='style.css'/>
-    <title>Learning Platform</title>
+    <title>Virtual Learning Environment</title>
   </head>
   <body>
     <img src="http://i1061.photobucket.com/albums/t480/ericqweinstein/php-logo_zps408c82d7.png"/>
     <div class="header"><h1>
       <?php
-<<<<<<< HEAD
-      $welcome = "Welcome to the Learning Platform!";
-=======
-      $welcome = "Welcome to Virtual Learning Place";
->>>>>>> FETCH_HEAD
+      $welcome = "Welcome to Virtual Learning Environment!";
       echo $welcome;
       ?>
     </h1></div>
 
     <form>
       <div class="form-group">
-        <label for="exampleInputEmail1">This Email</label>
+        <label for="exampleInputEmail1">Email</label>
         <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
       </div>
       <div class="form-group">
