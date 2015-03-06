16fe71930d2c508ca905c56def3a05c22cfe3971
Sarah Alkulaibi <sarah.alkulaibi.11@ucl.ac.uk>
2015-03-06 14:23:54 +0000

    original file

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
     <img src="http://i1061.photobucket.com/albums/t480/ericqweinstein/php-logo_zps408c82d7.png"/>
     <div class="header"><h1>
       <?php
-      $welcome = "Welcome to Virtual Learning Place";
+      $welcome = "Welcome to Virtual Learning Environment";
       echo $welcome;
       ?>
     </h1></div>
 
     <form>
       <div class="form-group">
-        <label for="exampleInputEmail1">This Email</label>
+        <label for="exampleInputEmail1">Email</label>
         <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
       </div>
       <div class="form-group">
