d6fc0de3c11f2b4f1c929de504e8c5aaf0624787
ttai <ttai@wellesley.edu>
2015-03-08 15:28:19 +0000

    created admin class page with tabs

---
+++ adminClassPage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="3333.png">
+  <link type='text/css' rel='stylesheet' href='style.css'/>
+  
+  <title>Class Page</title>
+</head>
+
+  <body>
+  
+    <div id = "header"><h1>
+    <p>Welcome!</p>
+    </h1>
+    <h3>
+      
+    </h3>
+    </div>
+  <div id="menu">
+    <ul>
+      <li><a href ="#" title = "Announcements and Forum" class="active">Announcements and Forum</a></li>
+      <li><a href ="#" title = "Upload Your Report">Enrolled Students</a></li>
+      <li><a href ="#" title = "Submit Peer Assessments">Manage Project Groups</a></li>
+      <li><a href ="#" title = "Assessments of Your Report">View Grades and Rankings</a></li>
+      <li><a href ="#" title = "Your Grades">Your Grades</a></li>
+    </ul>
+  </div>
+
+  <div id="main">
+    <h1>Announcements and Forum</h1>
+    <p>Main Content</p>
+  </div>
+  </body>
+</html>
