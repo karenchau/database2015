19d648b98cb01167b3135b8d2111e999e8f832be
Sarah Alkulaibi <sarah.alkulaibi.11@ucl.ac.uk>
2015-03-08 13:09:18 +0000

    arranging + logout issue

--- index.php
+++ index.php
@@ -37,7 +37,8 @@ if (!isset($_SESSION['email'])) {
       <div class="form-group">
         <label for="exampleInputPassword1">Password</label>
         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <label></label>
         <form align="right" name="form1" method="post" action="logout.php">
-        <label class="logout"><input name="submit" type="submit" value="Logout"></label>
+        <label class="logout"><input name="submit" type="submit" value="logout"></label>
   </body>
 </html>
