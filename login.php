b1176c15cfe0e3ade34b150a6eccc13cfd305a0b
karenchau <kchau@smith.edu>
2015-03-09 17:44:42 +0000

    Revert b2144fd..043f87b
    This rolls back to commit b2144fde1f222168498092cf027d3ff17228fa0e.

--- login.php
+++ login.php
@@ -33,10 +33,9 @@ if (isset($_POST['signin'])) {
 	}
 } else if (isset($_POST['signup'])) { //If the user chooses to signup, then this would initiate this if statement
     if (isset($_POST['email']) || isset($_POST['password']) || isset($_POST['first_name']) || isset($_POST['last_name']) || isset($_POST['role'])) {
-        if (empty($_POST['email']) || empty($_POST['password']) || empty($_POST['first_name']) || empty($_POST['last_name']) || empty($_POST['role'])) {
+        if (empty($_POST['email']) || empty($_POST['password'])) {
             $signup_errors = 'Please fill all fields.';
-		} 
-		else {
+		} else {
             require('connect.php');
             $db = open_connection();
             $email = mysqli_real_escape_string($db, $_POST['email']);
@@ -55,7 +54,7 @@ if (isset($_POST['signin'])) {
 				return;
 			}
 		}
-	else {
+	} else {
 		unset($signup_errors);
 	}
 }
