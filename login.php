4aa7ed7fc125fc30a4e6abdbd0e19329b2c006d4
Sarah Alkulaibi <sarah.alkulaibi.11@ucl.ac.uk>
2015-03-09 17:36:46 +0000

    returned the code
    removing the if statement did not result in removing the warning
    regarding filling all the fields

--- login.php
+++ login.php
@@ -32,6 +32,7 @@ if (isset($_POST['signin'])) {
 		unset($signin_errors);
 	}
 } else if (isset($_POST['signup'])) { //If the user chooses to signup, then this would initiate this if statement
+    if (isset($_POST['email']) || isset($_POST['password']) || isset($_POST['first_name']) || isset($_POST['last_name']) || isset($_POST['role'])) {
         if (empty($_POST['email']) || empty($_POST['password']) || empty($_POST['first_name']) || empty($_POST['last_name']) || empty($_POST['role'])) {
             $signup_errors = 'Please fill all fields.';
 		} 
@@ -54,6 +55,9 @@ if (isset($_POST['signin'])) {
 				return;
 			}
 		}
+	else {
+		unset($signup_errors);
+	}
 }
 
 ?>
