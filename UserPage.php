98d2c79c2115949525b1049ebbd1fbb706a54834
karenchau <kchau@smith.edu>
2015-03-09 16:20:31 +0000

    fixed print syntax

--- UserPage.php
+++ UserPage.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['email'])) {
           $result = mysqli_query($db, $query);
           if (mysqli_num_rows($result) > 0) {
               $entry = mysqli_getresult($result, mysqli_num_rows($result), 0);
-              echo "$entry .'s ";
+              echo "$entry's ";
               mysqli_close($db);
               } else {
                   echo "User";
