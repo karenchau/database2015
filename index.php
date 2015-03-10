8b6efe2d1ce1d63bfd8e0590ba4393082ce2d5f4
karenchau <kchau@smith.edu>
2015-03-10 12:34:14 +0000

    updated index.php to reflect my changes on userpge

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ session_start();
 if (!isset($_SESSION['email'])) {
     header('Location: login.php');
     return;
-}?>
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,8 +16,8 @@ if (!isset($_SESSION['email'])) {
       <meta name="author" content="">
       <link rel="icon" href="3333.png">
       <link type='text/css' rel='stylesheet' href='style.css'/>
-      
-      <!-- Creating a personalized homepage greeting-->
+
+      <!-- Creating a personalized tab greeting-->
       <title>
           <?php
           require('connect.php');
@@ -24,8 +25,8 @@ if (!isset($_SESSION['email'])) {
           $query = "select first_name from user where email = '$_SESSION[email]' ";
           $result = mysqli_query($db, $query);
           if (mysqli_num_rows($result) > 0) {
-              $y = mysqli_result($result, mysqli_num_rows($result));
-              echo $y ."'s ";
+              $fname_entry = mysqli_getresult($result, mysqli_num_rows($result), 0);
+              echo "$fname_entry's ";
               mysqli_close($db);
               } else {
                   echo "User";
@@ -33,21 +34,36 @@ if (!isset($_SESSION['email'])) {
               }
               ?>
               Homepage</title>  
-              
-              
-
   </head>
 
   <body>
+      <!-- Creating a personalized homepage greeting-->
       <div id="header">
-          <h1>Platform</h1>
+          <h1>
+            <?php
+            echo "$fname_entry's ";
+            ?>
+            Platform</h1>
       </div>
 
       <div id="nav">
           London<br>
-          <a href="studentclasspage.php">COMP2015</a><br>
-          <a href="studentclasspage2.php">COMP4008</a><br>
-          <a href="classpage.php">Classes Page</a><br>
+          <?php
+            $db = open_connection();
+            $query = "select role from user where email = '$_SESSION[email]' ";
+            $result = mysqli_query($db, $query);
+            $role = mysqli_getresult($result, mysqli_num_rows($result), 0);
+            if($role) {                                                           //depending on the role, a different page will appear
+              echo "<a href=\"adminClassPage.php\">COMP2015</a><br>";
+            } else {
+              echo "<a href=\"studentClassPage.php\">COMP3019</a><br>";
+              echo "<a href=\"adminClassPage.php\">admin</a><br>";
+            }
+            $_SESSION['isAdmin'] = $role;
+            mysqli_close($db);
+            ?>
+          <a href="studentClassPage.php">COMP9999</a><br>
+          <a href="studentClassPage2.php">COMP4008</a><br>
 
       </div>
 
@@ -60,21 +76,28 @@ if (!isset($_SESSION['email'])) {
               Standing on the River Thames, London has been a major settlement for two millennia.
           </p>
           <?php 
-          function mysqli_result($res, $row, $field=0) { 
+              function mysqli_getresult($res, $row, $field) { //takes the row (expects only one row since a primary key is used) and prints out all the fields
               $res->data_seek($row); 
-              $datarow = $res->fetch_array(); 
-              return $datarow[$field]; 
-          }
+              $datarow = $res->fetch_array();
+              if($res->field_count == 1) {
+                return $datarow[$field];
+              } else {
+                while($field < $res->field_count) {
+                  echo $datarow[$field] ."<br>";
+                  $field++;
+                }
+              }
+            }
 
           $db = open_connection();
-          $query = "select first_name from user where email = '$_SESSION[email]' ";
+          $query = "select * from user where email = '$_SESSION[email]' ";
           $result = mysqli_query($db, $query);
-          $z = mysqli_result($result, mysqli_num_rows($result));
-          echo $z;
+          mysqli_getresult($result, mysqli_num_rows($result), 0);
           $num = mysqli_num_rows($result);
           echo $num;
           mysqli_close($db);
           ?>
+          <p><a href="logout.php">Logout link (as an alternative for now)</a></p>
           <p class="navbar-btn"><a href="logout.php" class="btn btn-danger">Sign out</a></p>
       </div>
 
