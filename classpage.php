da2a60aa64958b029f423b0587ca034d153c055d
Sarah Alkulaibi <sarah.alkulaibi.11@ucl.ac.uk>
2015-03-11 11:54:32 +0000

    edit

--- classpage.php
+++ classpage.php
@@ -83,14 +83,15 @@ if (!isset($_SESSION['email'])) {
                         ?>
 
 
-                </ul>
+                    </ul>
                 </div>
-                <!-- right colomn -->
-                <!-- Tabs for navigating the user options-->    
-                <ul class="nav nav-tabs">
-                <li role="presentation" class="active"><a href="classpage.php">Class List</a></li>
-                <li role="presentation"><a href="profilepage.php">Profile</a></li>
-                <li role="presentation"><a href="forumpage.php">Forum</a></li>
+                    <!-- right colomn -->
+                    <!-- Tabs for navigating the user options-->    
+                    <ul class="nav nav-tabs">
+                    <li role="presentation" class="active"><a href="classpage.php">Class List</a></li>
+                    <li role="presentation"><a href="profilepage.php">Profile</a></li>
+                    <li role="presentation"><a href="forumpage.php">Forum</a></li>
+                    </ul>
             </div>
         </div>
     </body>
