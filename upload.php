c1b54f25d4cfe256acd17d59071023a32443e970
ttai <ttai@wellesley.edu>
2015-03-13 18:48:37 +0000

    added upload code back

--- upload.php
+++ upload.php
@@ -6,6 +6,50 @@ if (!isset($_SESSION['email'])) {
 }
 
 echo 'hi';
+if (isset($_FILES['file'])){
+	//make sure file was uploaded without errors
+	/*
+	if ($_FILES['file'][['error']] == 0) {
+		//Connect to database
+		require('connect.php');
+		$db = open_connection();
+		$email = mysqli_real_escape_string($db, $_SESSION['email']);
+        $query = "select group from user where email = '$email' ";
+        $result = mysqli_query($db, $query);
+        require('functions.php');
+        if (mysqli_num_rows($result) > 0) {
+          $fname_entry = mysqli_getresult($result, mysqli_num_rows($result), 0);
+        } else {
+          $fname_entry = "User";
+        }
+	}
+	*/
+
+	$file_name = $db->mysql_real_escape_string($_FILES['file']['name']);
+	$file_type = $db->mysql_real_escape_string($_FILES['file']['type']);
+	$content = $db->mysql_real_escape_string(file_get_contents(($_FILES['file']['tmp_name'])));
+	$file_size = intval($_FILES['file']['size']);
+
+	$query = "INSERT INTO 'report'('name', 'type', 'size', 'data', 'group', 'uploadtime') VALUES('{$file_name}', '{$file_type}', '{$file_size}', '{content}', '1', NOW())";
+
+	$result = $db->query($query);
+
+	if ($result) {
+		echo 'Your file was successfully uploaded.';
+	} else {
+		echo 'ERROR: failed to insert file.' . "<pre>{$db->error}</pre>";
+	}
+	else {
+		echo 'An error occurred while the file was being uploaded.' . 'Error code: ' . intval($_FILES[file]['error']);
+	}
+	mysql_close($db);
+}
+else {
+	echo 'Error: A file was not sent';
+}
+
+//link to go back
+echo '<p>Click <a href = "index.php">here</a> to go back </p>';
 
 
 ?>
