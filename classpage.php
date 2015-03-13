987d1f361019e1dc213f8685d76f09b4ab4e8f89
karenchau <kchau@smith.edu>
2015-03-13 10:51:23 +0000

    took out div tag

--- classpage.php
+++ classpage.php
@@ -112,20 +112,20 @@ if (!isset($_SESSION['email'])) {
                 </div>
                     <!-- right colomn -->
                     <!-- Tabs for navigating the user options-->    
-                    <div class="container">
-                    	<ul class="nav nav-tabs">
-                    		<li class="nav active"><a href="#A" data-toggle="tab">A</a></li>
-                    		<li class="nav"><a href="#B" data-toggle="tab">B</a></li>
-                    		<li class="nav"><a href="#C" data-toggle="tab">C</a></li>
-                    	</ul>
+                
+                	<ul class="nav nav-tabs">
+                		<li class="nav active"><a href="#A" data-toggle="tab">A</a></li>
+                		<li class="nav"><a href="#B" data-toggle="tab">B</a></li>
+                		<li class="nav"><a href="#C" data-toggle="tab">C</a></li>
+                	</ul>
 
-                    	<!-- Tab panes -->
-                    	<div class="tab-content">
-                    		<div class="tab-pane fade in active" id="A">Content inside tab A</div>
-                    		<div class="tab-pane fade" id="B">Content inside tab B</div>
-                    		<div class="tab-pane fade" id="C">Content inside tab C</div>
-                    	</div>
-                    </div>
+                	<!-- Tab panes -->
+                	<div class="tab-content">
+                		<div class="tab-pane fade in active" id="A">Content inside tab A</div>
+                		<div class="tab-pane fade" id="B">Content inside tab B</div>
+                		<div class="tab-pane fade" id="C">Content inside tab C</div>
+                	</div>
+
             </div>
         </div>
     </body>
