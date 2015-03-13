ad240e8eeb6fa53c4be18add4bfd9289950ec4e8
karenchau <kchau@smith.edu>
2015-03-13 18:21:16 +0000

    testing enroll.php

--- enroll.php
+++ enroll.php
@@ -5,11 +5,35 @@
 	$db = open_connection();
 	$query = "select * from user";
 	$result = mysqli_query($db, $query);
-	while($row = $result->fetch_array()) {
-		echo $row['first_name'] . " " . $row['last_name'];
-  		echo "<br />";
+	$data = array();
+	while($row = mysqli_fetch_assoc($result)) {
+		$data[] = $row;
 	}
+	$colNames = array_keys(reset($data))
 	mysqli_close($db);
 ?>
+<table border="1">
+ <tr>
+    <?php
+       //print the header
+       foreach($colNames as $colName)
+       {
+          echo "<th>$colName</th>";
+       }
+    ?>
+ </tr>
 
+    <?php
+       //print the rows
+       foreach($data as $row)
+       {
+          echo "<tr>";
+          foreach($colNames as $colName)
+          {
+             echo "<td>".$row[$colName]."</td>";
+          }
+          echo "</tr>";
+       }
+    ?>
+ </table>
 <p>end</p>
