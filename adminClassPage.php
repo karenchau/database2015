5ed1f2d87766c1d314864eb401bc1bc0bb4ad6d7
karenchau <kchau@smith.edu>
2015-03-13 21:26:08 +0000

    redirecting message

--- adminClassPage.php
+++ adminClassPage.php
@@ -55,7 +55,7 @@ if (!isset($_SESSION['email'])) {
       </div>
       <?php
         if (!$_SESSION['isAdmin']) {
-          $adminerror = "You do not have the privileges to view this page.";
+          $adminerror = "You do not have the privileges to view this page. <br>Redirecting to student class page . . .";
           echo "<div class=\"alert alert-danger\" role=\"alert\">$adminerror</div>";
           header("refresh:4; url=studentClassPage.php");
           return;
