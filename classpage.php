4ca09aa348596947fd5f181a606774d1b3b364a1
karenchau <kchau@smith.edu>
2015-03-13 11:31:52 +0000

    updated text

--- classpage.php
+++ classpage.php
@@ -111,18 +111,18 @@ if (!isset($_SESSION['email'])) {
                     <!-- right colomn -->
                     <!-- Tabs for navigating the user options-->    
                 <div class="col-sm-9">
-                	<div class="container">
+                	<div class="col-sm-12">
                 		<ul class="nav nav-tabs">
-                			<li class="nav active"><a href="#A" data-toggle="tab">A</a></li>
-                			<li class="nav"><a href="#B" data-toggle="tab">B</a></li>
-                			<li class="nav"><a href="#C" data-toggle="tab">C</a></li>
+                			<li class="nav active"><a href="#classpage.php" data-toggle="tab">Classes</a></li>
+                			<li class="nav"><a href="#profilepage.php" data-toggle="tab">Profile</a></li>
+                			<li class="nav"><a href="#forum.php" data-toggle="tab">Forum</a></li>
                 		</ul>
 
                 		<!-- Tab panes -->
                 		<div class="tab-content">
-                			<div class="tab-pane fade in active" id="A">Content inside tab A</div>
-                			<div class="tab-pane fade" id="B">Content inside tab B</div>
-                			<div class="tab-pane fade" id="C">Content inside tab C</div>
+                			<div class="tab-pane fade in active" id="classpage.php">Content inside class tab</div>
+                			<div class="tab-pane fade" id="profilepage.php">Content inside profile tab</div>
+                			<div class="tab-pane fade" id="forum.php">Content inside forum tab</div>
                 		</div>
                 	</div>
                 </div>
