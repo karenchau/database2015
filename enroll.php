efc5c85fa9970803217c709831af804a5a80ceb2
karenchau <kchau@smith.edu>
2015-03-15 16:19:35 +0000

    spacing

--- enroll.php
+++ enroll.php
@@ -1,6 +1,6 @@
 <html>
 	<h3>All students</h3>
-
+	<br>
 	<?php
 		$db = open_connection();
 		$query = "SELECT first_name, last_name, email, department, year from user where role = '0' ";
@@ -8,8 +8,14 @@
 		print_table($result);
 		mysqli_close($db);
 	?>
-
+	<br><br>
 	<h3>All students registered for this class</h3>
-
-	
+	<?php
+		$db = open_connection();
+		$query = "SELECT student_id from enrolled_list where class = '$_SESSION[class]' ";
+		$result = mysqli_query($db, $query);
+		print_table($result);
+		mysqli_close($db);
+	?>
+	<br><br>
 </html>
