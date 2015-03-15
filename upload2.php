883159eb4153c8fe69d6d8c78207e67d3cfabe2b
Sarah Alkulaibi <sarah.alkulaibi.11@ucl.ac.uk>
2015-03-15 13:55:41 +0000

    edit

--- upload2.php
+++ upload2.php
@@ -46,11 +46,11 @@ if(isset($_FILES['uploaded_file'])) {
  
         // Create the SQL query
         $query = " INSERT INTO report (id, name, type, size, data, group, uploadtime, class) 
-                    VALUES ('{$id}', '{$name}', '{$type}', '{$size}', '{$data}', '{$group}', '{$date}' ,'{$class}')";
+                    VALUES ('$id', '$name', '$type', '$size', '$data', '$group', '$date' ,'$class')";
  
         // Execute the query
         $result = mysqli_query($db, $query);
-        echo 'got result, again';
+        echo '<p>got result, again</p>';
  
         // Check if it was successfull
         if($result) {
