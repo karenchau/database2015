cebca99460d3e6b91b6b377c166d64dc54229fb3
ttai <ttai@wellesley.edu>
2015-03-15 17:02:47 +0000

    debug twitching collapsible accordion
    removed jquery js

--- studentClassPage.php
+++ studentClassPage.php
@@ -22,10 +22,7 @@ $_SESSION['class'] = $_GET['classid'];
 
     <!-- Custom styles for this template -->
     <link href="css/main.css" rel="stylesheet">
-
-    <!-- Latest compiled and minified JQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-
+    
     <!-- Latest compiled and minified JavaScript -->
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
