55c61ad55fca218169e96f737de6e384be3b3ec1
karenchau <kchau@smith.edu>
2015-03-15 15:48:15 +0000

    will select only students

--- enroll.php
+++ enroll.php
@@ -2,7 +2,7 @@
 
 <?php
 	$db = open_connection();
-	$query = "select first_name, last_name, email, department, year, role from user";
+	$query = "SELECT first_name, last_name, email, department, year from user where role = '0' ";
 	$result = mysqli_query($db, $query);
 	print_table($result);
 	mysqli_close($db);
