795974b545d91575e422676bf14aef410dda5a11
karenchau <kchau@smith.edu>
2015-03-15 19:25:08 +0000

    spacing

--- enroll.php
+++ enroll.php
@@ -15,13 +15,11 @@
 	?>
 	<br>
 	<h3>Enroll a student</h3>
-	<div class="col-lg-6">
-		<div class="input-group">
-			<input type="text" class="form-control" placeholder="Enter email address">
-			<span class="input-group-btn">
-				<button class="btn btn-default" type="button">Go!</button>
-			</span>
-		</div>
+	<div class="input-group">
+		<input type="text" class="form-control" placeholder="Enter email address">
+		<span class="input-group-btn">
+			<button class="btn btn-default" type="button">Go!</button>
+		</span>
 	</div>
 	
 
