6f9182d76a181478776c70c987956c74d6699528
Sarah Alkulaibi <sarah.alkulaibi.11@ucl.ac.uk>
2015-03-15 19:03:20 +0000

    edit

--- uploading.php
+++ uploading.php
@@ -20,7 +20,7 @@ if(isset($_FILES['uploaded_file'])) {
         $class = mysqli_real_escape_string($db, $_SESSION['class']);
         
         //more than one member in the group 
-        $query = " SELECT group_id FROM group WHERE group.class ='$class' AND ((group.member1 = '$email' OR group.member2 = '$email') OR (group.member3 = '$email'))";
+        $query = " SELECT group_id FROM group WHERE (group.class ='$class') AND ((group.member1 = '$email' OR group.member2 = '$email') OR (group.member3 = '$email'))";
         
         $result = $db->query($query);
         
