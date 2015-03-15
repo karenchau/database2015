395aac925465d567fcdde9c8cc71a2696db9f1e7
ttai <ttai@wellesley.edu>
2015-03-15 15:06:42 +0000

    evaluations tab

---
+++ evaluation.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="3333.png">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- Latest compiled and minified JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+    <script>
+      $(function() {
+        $("#accordion").accordion({
+        collapsible: true
+        });
+      });
+    </script>
+  </head>
+
+  <body>
+  <!--get array? of groups to assess-->
+
+  <!--display group reports + evaluation forms-->
+    <div id="accordion">
+      <h3>Section 1</h3>
+      <div>
+        <!-- provide download link for report -->
+        <!-- provide evaluation form for corresponding report -->
+      </div>
+
+      <h3>Section 2</h3>
+      <div>
+      </div>
+
+      <h3>Section 3</h3>
+      <div>
+      </div>
+
+      <h3>Section 4</h3>
+      <div>
+      </div>
+
+      <h3>Section 5</h3>
+      <div>
+      </div>
+    </div>
+
+  </body>
+</html>
