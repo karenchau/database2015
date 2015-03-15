509dcc03bdb96e4541fa2475d8870927608dd93b
karenchau <kchau@smith.edu>
2015-03-15 19:10:06 +0000

    button

--- enroll.php
+++ enroll.php
@@ -20,7 +20,7 @@
 			<input class="span2" id="appendedInputButton" name="search_term" type="text">
 			<input class="span2" id="search_type" name="search_type" type="hidden">
 			<div class="btn-group">
-				<button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+				<button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
 					Action
 					<span class="caret"></span>
 				</button>
