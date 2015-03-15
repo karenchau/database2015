c11622fe86d006abc3ab20d37485ee4bb8c4c3d7
karenchau <kchau@smith.edu>
2015-03-15 18:19:43 +0000

    updated user tab

--- users.php
+++ users.php
@@ -11,27 +11,35 @@
 			$no_studentuser_error = "No one is registered as a student user on Platform yet.";
 			echo "<div class=\"alert alert-danger\" role=\"alert\">$no_studentuser_error</div>";
 		}
-	?>
-	<input type="text" id="searchbar" placeholder="Type in a student's first name."></input>
-	<script> 
-		$("#searchbar").on("keyup", function() {
-			var value = $(this).val();
+		?>
+		<input type="text" id="searchbar" placeholder="Type in a student's first name."></input>
+		<script> 
+			$("#search").on("keyup", function() {
+				var value = $(this).val().toLowerCase();
 
-			$("table tr").each(function(index) {
-				if (index !== 0) {
+				$("table tr").each(function(index) {
+					if (index !== 0) {
 
-					$row = $(this);
+						$row = $(this);
 
-					var id = $row.find("td:first").text();
+						var id = $row.find("td:first").text();
+						var id2 = $row.find("td:nth-child(2)").text();
+						var id3 = $row.find("td:nth-child(3)").text();
+						var id4 = $row.find("td:nth-child(4)").text();
+						var id5 = $row.find("td:nth-child(5)").text();
 
-					if (id.indexOf(value) !== 0) {
-						$row.hide();
+						if (id.toLowerCase().indexOf(value) !== 0 && 
+							id2.toLowerCase().indexOf(value) !== 0 && 
+							id3.toLowerCase().indexOf(value) !== 0 && 
+							id4.toLowerCase().indexOf(value) !== 0 && 
+							id5.toLowerCase().indexOf(value) !== 0 &&) {
+							$row.hide();
+						}
+						else {
+							$row.show();
+						}
 					}
-					else {
-						$row.show();
-					}
-				}
+				});
 			});
-		});
-	</script>
+		</script>
 </html>
