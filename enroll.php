36f8195cc6241c37a1fc5de412b2d89853957d59
karenchau <kchau@smith.edu>
2015-03-15 15:07:11 +0000

    udated enroll

--- enroll.php
+++ enroll.php
@@ -5,7 +5,8 @@
 	$db = open_connection();
 	$query = "select * from user";
 	$result = mysqli_query($db, $query);
-
+	echo "here";
+	require('functions.php');
 	print_table($result);
 	mysqli_close($db);
 ?>
