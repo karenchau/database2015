e7387576104c2aa59329602ac766c5ba2cf3edf6
karenchau <kchau@smith.edu>
2015-03-15 19:39:37 +0000

    enroll page

--- enroll.php
+++ enroll.php
@@ -23,8 +23,8 @@
 	</div>
 	<?php
 		if (isset($_POST['enroll'])) {
-			if (empty($_POST['studentemail']) {
-
+			if (isset($_POST['studentemail']) || empty($_POST['studentemail']) {
+				$enroll_errors = 'Error!: Please enter an email address';
 			}
 		}
 		mysqli_close($db);
