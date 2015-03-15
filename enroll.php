5c8ba8669f631dcbed94c263de524f809a31b660
karenchau <kchau@smith.edu>
2015-03-15 13:12:11 +0000

    added function2

--- enroll.php
+++ enroll.php
@@ -5,35 +5,10 @@
 	$db = open_connection();
 	$query = "select * from user";
 	$result = mysqli_query($db, $query);
-	$data = array();
-	while($row = mysqli_fetch_assoc($result)) {
-		$data[] = $row;
-	}
-	$colNames = array_keys(reset($data));
+	
+	require('functions.php');
+	print_table($result);
+	mysqli_close($db);
 ?>
-<table border="1">
- <tr>
-    <?php
-       //print the header
-       foreach($colNames as $colName)
-       {
-          echo "<th>$colName</th>";
-       }
-    ?>
- </tr>
 
-    <?php
-       //print the rows
-       foreach($data as $row)
-       {
-          echo "<tr>";
-          foreach($colNames as $colName)
-          {
-             echo "<td>".$row[$colName]."</td>";
-          }
-          echo "</tr>";
-       }
-    mysqli_close($db);
-    ?>
- </table>
 <p>end</p>
