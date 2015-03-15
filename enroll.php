ca15f5aac3b3aa207183bf002fa0bd3e6b7ec006
karenchau <kchau@smith.edu>
2015-03-15 17:35:20 +0000

    added search bar

--- enroll.php
+++ enroll.php
@@ -9,18 +9,44 @@
 			print_table($result);
 		} else {
 			$no_studentuser_error = "No one is registered as a student user on Platform yet.";
-          	echo "<div class=\"alert alert-danger\" role=\"alert\">$no_studentuser_error</div>";
+			echo "<div class=\"alert alert-danger\" role=\"alert\">$no_studentuser_error</div>";
 		}
-	echo "<br>";
-	echo "<h3>All students registered for this class</h3>";
-	echo "<br>";
+	?>
+	<input type="text" id="searchbar" placeholder="  live search"></input>
+	<script> 
+		$("#searchbar").on("keyup", function() {
+			var value = $(this).val();
+
+			$("table tr").each(function(index) {
+				if (index !== 0) {
+
+					$row = $(this);
+
+					var id = $row.find("td:first").text();
+
+					if (id.indexOf(value) !== 0) {
+						$row.hide();
+					}
+					else {
+						$row.show();
+					}
+				}
+			});
+		});
+	</script>
+
+	<br>
+	<h3>All students registered for this class</h3>
+	<br>
+	<?php
+		// This nested query first finds all the students enrolled in the class and then uses those results to find their names.
 		$query2 = "SELECT first_name, last_name from user where email in (SELECT student_id from enrolled_list where class = '$_SESSION[class]')";
 		$result2 = mysqli_query($db, $query2);
 		if (mysqli_num_rows($result2) > 0) {
 			print_table($result2);
 		} else {
 			$reg_none_error = "No one is registered for this class yet.";
-          	echo "<div class=\"alert alert-danger\" role=\"alert\">$reg_none_error</div>";
+			echo "<div class=\"alert alert-danger\" role=\"alert\">$reg_none_error</div>";
 		}
 		mysqli_close($db);
 	?>
