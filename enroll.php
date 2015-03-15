f9c302357c9d1aeb363f97ed7e245749b3faef60
karenchau <kchau@smith.edu>
2015-03-15 16:26:00 +0000

    second table

--- enroll.php
+++ enroll.php
@@ -12,7 +12,7 @@
 	<h3>All students registered for this class</h3>
 	<?php
 		$db = open_connection();
-		$query = "SELECT student_id from enrolled_list where class = '$_SESSION[class]' ";
+		$query = "SELECT a.first_name, a.last_name, b.student_id from user a, enrolled_list b where b.class = '$_SESSION[class]' ";
 		$result = mysqli_query($db, $query);
 		print_table($result);
 		mysqli_close($db);
