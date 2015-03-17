a8f7296f137d0780be179b2b3d743637a9b199f9
karenchau <kchau@smith.edu>
2015-03-17 17:34:18 +0000

    testing

--- enroll.php
+++ enroll.php
@@ -15,5 +15,26 @@
 		mysqli_close($db);
 	?>
 
-
+	<div class="container">
+		<h2>Horizontal form with static control</h2>
+		<form class="form-horizontal" role="form">
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="email">Email:</label>
+				<div class="col-sm-10">
+					<p class="form-control-static">someone@example.com</p>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-sm-2" for="pwd">Password:</label>
+				<div class="col-sm-10">          
+					<input type="password" class="form-control" id="pwd" placeholder="Enter password">
+				</div>
+			</div>
+			<div class="form-group">        
+				<div class="col-sm-offset-2 col-sm-10">
+					<button type="submit" class="btn btn-default">Submit</button>
+				</div>
+			</div>
+		</form>
+	</div>
 </html>
