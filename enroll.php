d89aebfec7e0e755ff61520eaa7a591dfb3cd93e
karenchau <kchau@smith.edu>
2015-03-17 17:32:28 +0000

    adding it back

--- enroll.php
+++ enroll.php
@@ -1,4 +1,19 @@
 <html>
 	<h3>All students registered for this class</h3>
+	<br>
+	<?php
+		$db = open_connection();
+		// This nested query first finds all the students enrolled in the class and then uses those results to find their names.
+		$query = "SELECT first_name, last_name, email from user where email in (SELECT student_id from enrolled_list where class = '$_SESSION[class]')";
+		$result = mysqli_query($db, $query);
+		if (mysqli_num_rows($result) > 0) {
+			print_table($result);
+		} else {
+			$reg_none_error = "No one is registered for this class yet.";
+			echo "<div class=\"alert alert-danger\" role=\"alert\">$reg_none_error</div>";
+		}
+		mysqli_close($db);
+	?>
+
 
 </html>
