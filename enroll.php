1eb0713f2a0cf48caa7e92c68604e7114815cad6
karenchau <kchau@smith.edu>
2015-03-17 17:05:12 +0000

    out

--- enroll.php
+++ enroll.php
@@ -15,28 +15,7 @@
 		mysqli_close($db);
 	?>
 	<br>
-	<div class="container">
-  <h2>Horizontal form with static control</h2>
-  <form class="form-horizontal" role="form">
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="email">Email:</label>
-      <div class="col-sm-10">
-        <p class="form-control-static">someone@example.com</p>
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="pwd">Password:</label>
-      <div class="col-sm-10">          
-        <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-      </div>
-    </div>
-    <div class="form-group">        
-      <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-default">Submit</button>
-      </div>
-    </div>
-  </form>
-</div>
+
 	<!--<?php
 		/*If the admin chooses the enroll option, then it would initiate this if statement
 		if (isset($_POST['enrolled'])) {
