1160be8d9d95c7824259ae608838eaa3db797cf4
karenchau <kchau@smith.edu>
2015-03-17 17:03:31 +0000

    testing

--- enroll.php
+++ enroll.php
@@ -12,27 +12,31 @@
 			$reg_none_error = "No one is registered for this class yet.";
 			echo "<div class=\"alert alert-danger\" role=\"alert\">$reg_none_error</div>";
 		}
+		mysqli_close($db);
 	?>
 	<br>
-	<h3>Enroll a student</h3>
-	<Form name ="form1" Method ="POST" Action ="basicForm.php">
-<Input Type = "text" Value ="username" Name ="username">
-<Input Type = "Submit" Name = "Submit1" Value = "Login">
-</FORM>
-
-<?PHP
-if (isset($_POST['Submit1'])) {
-$username = $_POST['username'];
-
-if ($username = = "letmein") {
-print ("Welcome back, friend!");
-}
-else {
-print ("You're not a member of this site");
-}
-}
-	mysqli_close($db);
-?>
+	<div class="container">
+  <h2>Horizontal form with static control</h2>
+  <form class="form-horizontal" role="form">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Email:</label>
+      <div class="col-sm-10">
+        <p class="form-control-static">someone@example.com</p>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Password:</label>
+      <div class="col-sm-10">          
+        <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+      </div>
+    </div>
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+  </form>
+</div>
 	<!--<?php
 		/*If the admin chooses the enroll option, then it would initiate this if statement
 		if (isset($_POST['enrolled'])) {
