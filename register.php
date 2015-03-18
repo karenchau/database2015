88169cdb6b6174940143da620ca7869a748ca6fc
karenchau <kchau@smith.edu>
2015-03-18 15:15:53 +0000

    testing

--- register.php
+++ register.php
@@ -10,7 +10,7 @@
 		if (mysqli_num_rows($result) == 0) {
 			$enroll_errors = 'Error!: This student is not registered as a user on Platform yet.';
 		} else {
-			$query2 = "SELECT * from enrolled_list where student_id = '$student_email' ";
+			$query2 = "SELECT * from enrolled_list where student_id = '$student_email' and class = '$_SESSION[class]' ";
 			$result2 = mysqli_query($db, $query2);
 			if (mysqli_num_rows($result2) > 0) {
 				$enroll_errors = 'Error!: This student is already registered for this class.';
