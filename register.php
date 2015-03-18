ba49d3534c49a0d3c426e9b9e81257b06be2db99
karenchau <kchau@smith.edu>
2015-03-18 00:31:34 +0000

    changed back

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
 		require_once('connect.php');
 		$db = open_connection();
 		$student_email = mysqli_real_escape_string($db, $_POST['studentemail']);
-		$query = "SELECT * from user where email = 'ttai@mail.com' limit 1";
+		$query = "SELECT * from user where email = '$student_email' limit 1";
 		$result = mysqli_query($db, $query);
 		if (mysqli_num_rows($result) == 0) {
 			$enroll_errors = 'Error!: This student is not registered as a user on Platform yet.';
