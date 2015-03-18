8eb5523ee1320e752679d2f670daa552b04247c4
karenchau <kchau@smith.edu>
2015-03-18 00:17:57 +0000

    debugging

--- register.php
+++ register.php
@@ -1,25 +1,24 @@
 <?php
 	if (empty($_POST['studentemail'])) {
 		$enroll_errors = 'Error!: Please enter an email.';
-		$hasErr = TRUE;
+		$hasErr = 1;
 	} else {
 		$student_email = mysqli_real_escape_string($db, $_POST['studentemail']);
-		echo "$student_email";
 		$query = "SELECT * from user where email = '$student_email' limit 1";
 		$result = mysqli_query($db, $query);
 		if (mysqli_num_rows($result) == 0) {
 			$enroll_errors = 'Error!: This student is not registered as a user on Platform yet.';
-			$hasErr = TRUE;
+			$hasErr = 1;
 		} else {
 			unset($enroll_errors);
-			$hasErr = FALSE;
+			$hasErr = 0;
 		}
 	}
-	if ($hasErr) {
+	if ($hasErr == 1) {
 		echo json_encode(array('success' => false)); 
+		echo json_encode(array('emailz' => $student_email));
 	} else {
 		echo json_encode(array('success' => true)); 
 	}
 	unset($enroll_errors);
-	unset($hasErr);
 ?>
