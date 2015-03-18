02bec4e148aba33dd1c31b55f04b32587afe3f61
karenchau <kchau@smith.edu>
2015-03-18 15:41:45 +0000

    tsting

--- enroll.php
+++ enroll.php
@@ -27,7 +27,7 @@
 					success:function(data){
 						console.log(data);
 						var option = $.parseJSON (data);
-						//document.write(option["message"]);
+						document.write(option["message"]);
 						if(option["success"] == true) {
 							$("#success").show().fadeOut(5000); //=== Show Success Message==
 						} else {
