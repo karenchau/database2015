ebc8acb06b9cb2ddbdfcaae301df73fe724f4f96
karenchau <kchau@smith.edu>
2015-03-19 20:53:20 +0000

    debugging

--- studentgrade.php
+++ studentgrade.php
@@ -1,7 +1,7 @@
 <?php
   require_once('connect.php');
     $db = open_connection();
-    $query = "SELECT group_id, class, 1+(SELECT count(*) from temp_table_1 a WHERE a.average > b.average AND b.class = 'MATH2001') as rank, average FROM temp_table_1 b WHERE b.class = 'MATH2001' ";
+    $query = "SELECT group_id, class, 1+(SELECT count(*) from temp_table_1 a WHERE a.average > b.average AND a.class = 'MATH2001') as rank, average FROM temp_table_1 b WHERE b.class = 'MATH2001' ";
     $result = mysqli_query($db, $query);
     if (mysqli_num_rows($result) > 0) {
       print_table($result);
