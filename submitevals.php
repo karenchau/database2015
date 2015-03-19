6a763edfc84db947450c9be1e9fe3aa7a851863a
ttai <ttai@wellesley.edu>
2015-03-19 20:30:37 +0000

    test

--- submitevals.php
+++ submitevals.php
@@ -49,7 +49,7 @@ if (!isset($_POST['input_group']) OR $_POST['input_group'] == 'default') {
                 $overall = (int) ($criteria1 + $criteria2 + $criteria3 + $criteria4 + $criteria5);
                 $comments = mysqli_real_escape_string($db, $_POST['comments']);
                 
-                $query1 = "UPDATE evaluation SET comment='$comments', criteria1=$criteria1, criteria2=$criteria2, criteria3=$criteria3, criteria4=$criteria4, criteria5=$criteria5, WHERE id_report_group='$report_group' AND id_eval_group='$group_entry' AND class='$class'";
+                $query1 = "UPDATE evaluation SET comment='$comments', criteria1=$criteria1, criteria2=$criteria2, criteria3=$criteria3, criteria4=$criteria4, criteria5=$criteria5 WHERE id_report_group='$report_group' AND id_eval_group='$group_entry' AND class='$class'";
                 $result1 = mysqli_query($db, $query1);
                 $query2 = "UPDATE group_list SET grade=grade+$overall, num_groups=num_groups+1 WHERE group_id='$report_group' AND class='$class'";
                 $result2 = mysqli_query($db,$query2);
