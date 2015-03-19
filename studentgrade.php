f449d6433956cdb4848140b8c418fb2b290a1553
karenchau <kchau@smith.edu>
2015-03-19 22:00:40 +0000

    tseting

--- studentgrade.php
+++ studentgrade.php
@@ -2,17 +2,17 @@
     require_once('connect.php');
     require_once('functions.php');
     $db = open_connection();
-    $group = find_group($_SESSION['class'],'ttai@mail.com');
+    $group = find_group($_SESSION['class'],$_SESSION['email']);
     $query = "SELECT group_id, class, 1+(SELECT count(*) from temp_table_1 a WHERE a.average > b.average AND a.class = '$_SESSION[class]') as rank, average FROM temp_table_1 b WHERE b.class = '$_SESSION[class]' AND b.group_id = '$group' ";
     $result = mysqli_query($db, $query);
     if (mysqli_num_rows($result) > 0) {
       print_table($result);
-      echo "hereseessionwith find grou8";
+      echo "hereseessionwith find grou9";
       echo find_group('MATH2001','ttai@mail.com');
     } else {
       $reg_none_error = "No one is registered for this class yet.";
       echo "<div class=\"alert alert-danger\" role=\"alert\">$reg_none_error</div>";
-      echo "hereseessionwith find group8";
+      echo "hereseessionwith find group9";
       echo find_group('MATH2001','ttai@mail.com');
     }
     mysqli_close($db);
