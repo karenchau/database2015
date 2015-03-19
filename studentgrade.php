5d6296c671807fff397f36645cb6b38e7ee13c69
karenchau <kchau@smith.edu>
2015-03-19 21:55:07 +0000

    testing

--- studentgrade.php
+++ studentgrade.php
@@ -2,16 +2,17 @@
     require_once('connect.php');
     require_once('functions.php');
     $db = open_connection();
-    $query = "SELECT group_id, class, 1+(SELECT count(*) from temp_table_1 a WHERE a.average > b.average AND a.class = '$_SESSION[class]') as rank, average FROM temp_table_1 b WHERE b.class = '$_SESSION[class]' AND b.group_id = "find_group('MATH2001','ttai@mail.com')" ";
+    $group = find_group('MATH2001','ttai@mail.com');
+    $query = "SELECT group_id, class, 1+(SELECT count(*) from temp_table_1 a WHERE a.average > b.average AND a.class = '$_SESSION[class]') as rank, average FROM temp_table_1 b WHERE b.class = '$_SESSION[class]' AND b.group_id = '$group' ";
     $result = mysqli_query($db, $query);
     if (mysqli_num_rows($result) > 0) {
       print_table($result);
-      echo "hereseessionwith find grou65";
+      echo "hereseessionwith find grou6";
       echo find_group('MATH2001','ttai@mail.com');
     } else {
       $reg_none_error = "No one is registered for this class yet.";
       echo "<div class=\"alert alert-danger\" role=\"alert\">$reg_none_error</div>";
-      echo "hereseessionwith find group65";
+      echo "hereseessionwith find group6";
       echo find_group('MATH2001','ttai@mail.com');
     }
     mysqli_close($db);
