d25472b2ba3a9e8101b8cf8d276867dbec61efe5
Sarah Alkulaibi <sarah.alkulaibi.11@ucl.ac.uk>
2015-03-19 19:30:13 +0000

    test viewing evals

--- viewMyEvals.php
+++ viewMyEvals.php
@@ -17,19 +17,19 @@
     $db = open_connection();
     $email = mysqli_real_escape_string($db, $_SESSION['email']);
     $class = mysqli_real_escape_string($db, $_SESSION['class']);
-    //get user's group_id
-    $query = "SELECT * FROM group_list "; 
-    $query .= "WHERE class = '$class' "; 
-    $query .= "AND (member1 = '$email' OR member2 = '$email' OR member3 = '$email')";
-    $result = mysqli_query($db, $query);
-    if (!$result){
+    //get user's group_id (report_group)
+    $query1 = "SELECT * FROM group_list "; 
+    $query1 .= "WHERE class = '$class' "; 
+    $query1 .= "AND (member1 = '$email' OR member2 = '$email' OR member3 = '$email')";
+    $result1 = mysqli_query($db, $query);
+    if (!$result1){
         echo 'Query failed : '.mysqli_error($db);
         $db->close();
         exit(0);
     }
-    $row = mysqli_fetch_assoc($result);
-    $group_id = $row['group_id'];
-    
+    $row1 = mysqli_fetch_assoc($result);
+    $report_group = $row['group_id'];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -75,52 +75,77 @@
         </div>
         <!-- Find all the evals for this group-->
         <?php
-            $query ="SELECT * FROM evaluation";
-            $query .="WHERE ";
+            $query2 ="SELECT * FROM evaluation";
+            $query2 .=" WHERE (class = '$class' ";
+            $query2 .=" AND id_report_group = '$report_group' )";
+            $result2 = mysqli_query($db, $query);
+            if (!$result2){
+                echo 'Query failed : '.mysqli_error($db);
+                $db->close();
+                exit(0);
+            }
             
         ?>
          <!-- start loop for the number of available evals for this groupid in this classid-->
         <?php
-        
-            //while(){
-                ?>
-            <!-- List group -->
-           
-            <!-- get the groupid of the group who did this eval -->
+            while($row2 = mysqli_fetch_assoc($result2))
+                {
+        ?>
             <!-- creating the eval criteria listing -->
             <div id="EvalForm" class="mainbox col-xs-12"> 
                 <div class="panel panel-primary">
                     <div class="panel-heading">
-                        <h3>Group #'s Evaluation</h3>
+                        <!-- get the groupid of the group who did this eval -->
+                        <?php
+                            $eval_group =$row2['id_eval_group'];
+                        ?>
+                        <h3>Group <?php echo $eval_group ?>'s Evaluation</h3>
                     </div>
                     <div class="panel-body">
                         <ul class="list-group">
                             <a href="#clarity" class="list-group-item">
                                 <h4 class="list-group-item-heading"><strong>Clarity</strong></h4>
-                                <p class="list-group-item-text"><p>The report is written in a clear and concise manner.</p></p>
+                                <p class="list-group-item-text">
+                                        <p>The report is written in a clear and concise manner.</p>
+                                        <p>Grade: <?php $row2['criteria1']; ?></p>
+                                </p>
                             </a>
                             <a href="#focus" class="list-group-item  ">
                                 <h4 class="list-group-item-heading"><strong>Focus</strong></h4>
-                                <p class="list-group-item-text"><p>The report has a clear argument and stays on topic.</p></p>
+                                <p class="list-group-item-text">
+                                    <p>The report has a clear argument and stays on topic.</p>
+                                    <p>Grade: <?php $row2['criteria2']; ?></p>
+                                </p>
                             </a>
                             <a href="#organization" class="list-group-item ">
                                 <h4 class="list-group-item-heading"><strong>Organization </strong></h4>
-                                <p class="list-group-item-text"><p>The report has a clear argument and stays on topic.</p></p>
+                                <p class="list-group-item-text">
+                                    <p>The report has a clear argument and stays on topic.</p>
+                                    <p>Grade: <?php $row2['criteria3']; ?></p>
+                                </p>
                             </a>
                             <a href="#analysis" class="list-group-item ">
                                 <h4 class="list-group-item-heading"><strong>Analysis</strong></h4>
-                                <p class="list-group-item-text"><p>The report supports its argument with strong valid evidence.</p></p>
+                                <p class="list-group-item-text">
+                                    <p>The report supports its argument with strong valid evidence.</p>
+                                    <p>Grade: <?php $row2['criteria4']; ?></p>
+                                </p>
                             </a>
                             <a href="#comment" class="list-group-item ">
                                 <h4 class="list-group-item-heading"><strong>Comment</strong></h4>
-                                <p class="list-group-item-text"><p>The report shows careful attention to detail.</p></p>
+                                <p class="list-group-item-text">
+                                    <p>The report shows careful attention to detail.</p>
+                                    <p>Comment:</p>
+                                    <p><?php $row2['comment']; ?></p>
+                                </p>
                             </a>
                         </ul>
                     </div>
                 </div>
             </div>
             <br>
-        <?php //}
+        <?php
+                }
         ?>
         
         <?php
