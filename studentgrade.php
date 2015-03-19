cae692cdfac5e7dab47fcf4d3419e17e0803c7a0
karenchau <kchau@smith.edu>
2015-03-19 21:34:18 +0000

    tesing

--- studentgrade.php
+++ studentgrade.php
@@ -1,11 +1,11 @@
 <?php
     require_once('connect.php');
     $db = open_connection();
-    $query = "SELECT group_id, class, 1+(SELECT count(*) from temp_table_1 a WHERE a.average > b.average AND a.class = 'MATH2001') as rank, average FROM temp_table_1 b WHERE b.class = 'MATH2001' AND b.group_id = '1' ";
+    $query = "SELECT group_id, class, 1+(SELECT count(*) from temp_table_1 a WHERE a.average > b.average AND a.class = '$_SESSION[class]') as rank, average FROM temp_table_1 b WHERE b.class = '$_SESSION[class]' AND b.group_id = '1' ";
     $result = mysqli_query($db, $query);
     if (mysqli_num_rows($result) > 0) {
       print_table($result);
-      echo "here";
+      echo "hereseession";
     } else {
       $reg_none_error = "No one is registered for this class yet.";
       echo "<div class=\"alert alert-danger\" role=\"alert\">$reg_none_error</div>";
