b1d028870b4dcef011cb0ba67f28d5a345c14956
karenchau <kchau@smith.edu>
2015-03-20 17:32:21 +0000

    testing grades

--- studentgrade.php
+++ studentgrade.php
@@ -5,7 +5,7 @@
     $group = find_group($_SESSION['class'],$_SESSION['email']);
     $query = "CREATE TABLE `temptable` (`group_id` VARCHAR(10) NOT NULL, `class` VARCHAR(10) NOT NULL, `average` DECIMAL(10) NOT NULL DEFAULT 0, `rank` INT NULL, PRIMARY KEY (`group_id`, `class`)) ";
     $result = mysqli_query($db, $query);
-    $query3 = "INSERT INTO temptable (group_id, class, average) VALUES ('1', 'MATH2001', '30.0') ";
+    $query3 = "INSERT INTO temptable (group_id, class, average) VALUES ('1', 'MATH2001', (SELECT grade from group_list where group_id = '$group' and class = '$_SESSION[class]')) ";
     $result3 = mysqli_query($db, $query3);
     $query2 = "SELECT group_id, class, 1+(SELECT count(*) from temptable a WHERE a.average > b.average AND a.class = '$_SESSION[class]') as rank, average FROM temptable b WHERE b.class = '$_SESSION[class]' AND b.group_id = '$group' ";
     $result2 = mysqli_query($db, $query2);
