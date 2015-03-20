bcc6d226aa3ff4efda228d55f8204a152b34887a
karenchau <kchau@smith.edu>
2015-03-20 20:09:07 +0000

    tseting

--- studentgrade.php
+++ studentgrade.php
@@ -8,7 +8,7 @@
     $query2 = "SELECT group_id, class, (grade/num_groups)as avg from group_list where class = '$_SESSION[class]' ";
     $result2 = mysqli_query($db, $query2);
     while ($row = mysqli_fetch_assoc($result2)) {
-      $query3 = "INSERT INTO temptable (group_id, class, average) VALUES ('here', 'bye', 9) ";
+      $query3 = "INSERT INTO temptable (group_id, class, average) VALUES ('$row[group_id]', '$row[class]', 9) ";
       $result3 = mysqli_query($db, $query3);
     }
     //$query2 = "SELECT group_id, class, 1+(SELECT count(*) from temptable a WHERE a.average > b.average AND a.class = '$_SESSION[class]') as rank, average FROM temptable b WHERE b.class = '$_SESSION[class]' AND b.group_id = '$group' ";
