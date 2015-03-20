160035305023bf82c5abbb84e32a4c881ff20a69
karenchau <kchau@smith.edu>
2015-03-20 18:02:22 +0000

    testing

--- studentgrade.php
+++ studentgrade.php
@@ -14,6 +14,7 @@
     } else {
       $reg_none_error = "No grades have been entered yet.";
       echo "<div class=\"alert alert-danger\" role=\"alert\">$reg_none_error</div>";
+      echo "40/3";
     }
     mysqli_close($db);
 ?>
