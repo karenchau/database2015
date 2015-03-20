e9e4a0e9c9c0a2fcc72c288d9d66207398cb56ac
karenchau <kchau@smith.edu>
2015-03-20 20:18:19 +0000

    testing

--- studentgrade.php
+++ studentgrade.php
@@ -8,18 +8,18 @@
     $query2 = "SELECT group_id, class, (grade/num_groups)as avg from group_list where class = '$_SESSION[class]' ";
     $result2 = mysqli_query($db, $query2);
     while ($row = mysqli_fetch_assoc($result2)) {
-      $query3 = "INSERT INTO temptable (group_id, class, average) VALUES ('$row[group_id]', '$row[class]', 9) ";
+      $query3 = "INSERT INTO temptable (group_id, class, average) VALUES ('$row[group_id]', '$row[class]', $row[avg]) ";
       $result3 = mysqli_query($db, $query3);
     }
-    //$query2 = "SELECT group_id, class, 1+(SELECT count(*) from temptable a WHERE a.average > b.average AND a.class = '$_SESSION[class]') as rank, average FROM temptable b WHERE b.class = '$_SESSION[class]' AND b.group_id = '$group' ";
-    //$result2 = mysqli_query($db, $query2);
-    //if (mysqli_num_rows($result2) > 0) {
-    //  print_table($result2);
-    //} else {
-    //  $reg_none_error = "No grades have been entered yet.";
-    //  echo "<div class=\"alert alert-danger\" role=\"alert\">$reg_none_error</div>";
-    //}
-    $query4 = "DROP TABLE `temptable`";
+    $query4 = "SELECT group_id, class, 1+(SELECT count(*) from temptable a WHERE a.average > b.average AND a.class = '$_SESSION[class]') as rank, average FROM temptable b WHERE b.class = '$_SESSION[class]' AND b.group_id = '$group' ";
+    $result4 = mysqli_query($db, $query4);
+    if (mysqli_num_rows($result4) > 0) {
+      print_table($result4);
+    } else {
+      $reg_none_error = "No grades have been entered yet.";
+      echo "<div class=\"alert alert-danger\" role=\"alert\">$reg_none_error</div>";
+    }
+    $query5 = "DROP TABLE `temptable`";
     //mysqli_query($db, $query4);
     mysqli_close($db);
 ?>
