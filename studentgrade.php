5a3abbff5220413e759c4f29dae6d9df44420517
karenchau <kchau@smith.edu>
2015-03-20 17:27:44 +0000

    tseting grades

--- studentgrade.php
+++ studentgrade.php
@@ -3,9 +3,11 @@
     require_once('functions.php');
     $db = open_connection();
     $group = find_group($_SESSION['class'],$_SESSION['email']);
-    $query = "CREATE TABLE `temp_table_2` (`group_id` VARCHAR(10) NOT NULL, `class` VARCHAR(10) NOT NULL, `average` DECIMAL(10) NOT NULL DEFAULT 0, `rank` INT NULL, PRIMARY KEY (`group_id`, `class`)) ";
+    $query = "CREATE TABLE `temptable` (`group_id` VARCHAR(10) NOT NULL, `class` VARCHAR(10) NOT NULL, `average` DECIMAL(10) NOT NULL DEFAULT 0, `rank` INT NULL, PRIMARY KEY (`group_id`, `class`)) ";
     $result = mysqli_query($db, $query);
-    $query2 = "SELECT group_id, class, 1+(SELECT count(*) from temp_table_1 a WHERE a.average > b.average AND a.class = '$_SESSION[class]') as rank, average FROM temp_table_1 b WHERE b.class = '$_SESSION[class]' AND b.group_id = '$group' ";
+    $query3 = "INSERT INTO temptable (group_id, class, average) VALUES ('1', 'MATH2001', '30.0') ";
+    $result3 = mysqli_query($db, $query3);
+    $query2 = "SELECT group_id, class, 1+(SELECT count(*) from temptable a WHERE a.average > b.average AND a.class = '$_SESSION[class]') as rank, average FROM temptable b WHERE b.class = '$_SESSION[class]' AND b.group_id = '$group' ";
     $result2 = mysqli_query($db, $query2);
     if (mysqli_num_rows($result2) > 0) {
       print_table($result2);
