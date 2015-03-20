684beb3f2e617ef1c464ffed437f310320c318dd
karenchau <kchau@smith.edu>
2015-03-20 20:04:17 +0000

    debuggng

--- studentgrade.php
+++ studentgrade.php
@@ -5,7 +5,7 @@
     $group = find_group($_SESSION['class'],$_SESSION['email']);
     $query = "CREATE TABLE `temptable` (`group_id` VARCHAR(10) NOT NULL, `class` VARCHAR(10) NOT NULL, `average` FLOAT NOT NULL DEFAULT 0, `rank` INT NULL, PRIMARY KEY (`group_id`, `class`)) ";
     $result = mysqli_query($db, $query);
-    $query2 = "SELECT group_id, class, (grade/num_groups)as avg from group_list where class = '$_SESSION[class]') ";
+    $query2 = "SELECT group_id, class, (grade/num_groups)as avg from group_list where class = '$_SESSION[class]' ";
     $result2 = mysqli_query($db, $query2);
     while ($row = mysqli_fetch_assoc($result2)) {
       $query3 = "INSERT INTO temptable (group_id, class, average) VALUES ('here', 'bye', 9) ";
