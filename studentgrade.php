d15c1f13d8729909d300bac581dec17673ec433d
karenchau <kchau@smith.edu>
2015-03-20 18:05:35 +0000

    testing

--- studentgrade.php
+++ studentgrade.php
@@ -3,7 +3,7 @@
     require_once('functions.php');
     $db = open_connection();
     $group = find_group($_SESSION['class'],$_SESSION['email']);
-    $query = "CREATE TEMPORARY TABLE `temptable` (`group_id` VARCHAR(10) NOT NULL, `class` VARCHAR(10) NOT NULL, `average` DECIMAL(10) NOT NULL DEFAULT 0, `rank` INT NULL, PRIMARY KEY (`group_id`, `class`)) ";
+    $query = "CREATE TABLE `temptable` (`group_id` VARCHAR(10) NOT NULL, `class` VARCHAR(10) NOT NULL, `average` DECIMAL(10) NOT NULL DEFAULT 0, `rank` INT NULL, PRIMARY KEY (`group_id`, `class`)) ";
     $result = mysqli_query($db, $query);
     $query3 = "INSERT INTO temptable (group_id, class, average) VALUES ('1', 'MATH2001', ( (SELECT grade from group_list where group_id = '$group' and class = '$_SESSION[class]')/ (SELECT num_groups from group_list where group_id = '$group' and class = '$_SESSION[class]') )) ";
     $result3 = mysqli_query($db, $query3);
@@ -14,7 +14,7 @@
     } else {
       $reg_none_error = "No grades have been entered yet.";
       echo "<div class=\"alert alert-danger\" role=\"alert\">$reg_none_error</div>";
-      echo "40/3";
+      echo "var_dump(40/3)";
     }
     mysqli_close($db);
 ?>
